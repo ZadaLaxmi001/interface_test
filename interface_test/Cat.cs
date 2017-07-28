307759352f25e86bcde34ebaa1a2cf1075dde367
Rashmi <rashmi.suresh@tismotech.com>
2017-07-28 15:29:22 +0530

    Add project files.

---
+++ interface_test/Cat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace interface_test
+{
+    public abstract class Cat
+    {
+        public abstract void Eat();
+        public abstract void Hunt();
+        public abstract void Sleep();
+    }
+}
---
+++ interface_test/Program.cs
@@ -0,0 +1,50 @@
+﻿using interface_test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace interface_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chetah cheetah = new Chetah();
+            cheetah.Hunt();
+            cheetah.Eat();
+            cheetah.Sleep();
+            cheetah.Purr();
+            Console.ReadLine();
+        }
+       
+
+     
+    }
+    interface IPurrable
+    {
+        void Purr();
+    }
+    public class Chetah : Cat, IPurrable
+    {
+        public override void Eat()
+        {
+            Console.WriteLine($"The cheetah eats.");
+        }
+
+        public override void Hunt()
+        {
+            Console.WriteLine($"The cheetah hunts.");
+        }
+
+        public void Purr()
+        {
+            Console.WriteLine($"The cheetah has purr.");
+        }
+
+        public override void Sleep()
+        {
+            Console.WriteLine($"The cheetah sleeps.");
+        }
+    }
+}
